d6d2e2a238a64a91b5acb123fa42a1d4bc957137
EC2 Default User <ec2-user@ip-172-31-0-192.us-east-2.compute.internal>
2018-06-01 14:22:14 +0000

    email set

--- config/initializers/amazon_ses.rb
+++ config/initializers/amazon_ses.rb
@@ -1,3 +1,3 @@
 ActionMailer::Base.add_delivery_method :ses, AWS::SES::Base,
-  access_key_id: AKIAIACK3KZX7F7GECFA,
-  secret_access_key: AlkUk112Ugtrji6hiIHXgWZo7TGngXCLyevwnD2L7Wuc
+  access_key_id: 'AKIAIACK3KZX7F7GECFA',
+  secret_access_key: 'AlkUk112Ugtrji6hiIHXgWZo7TGngXCLyevwnD2L7Wuc'
